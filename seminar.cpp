75477df7c50e2b3667315c763c0b5cf676d8a15b
Danil Domnich <domni4.d2@yandex.ru>
2025-03-06 14:58:28 +0300

    Добавить некоторые методы к Seminar

--- seminar.cpp
+++ seminar.cpp
@@ -1,18 +1,53 @@
 #include "seminar.hpp"
 #include "QStandardItemModel"
+#include <qdatetime.h>
 #include <vector>
+#include <algorithm>
 
 Seminar::Seminar(QString name)
 {
-    if (name.length() == 0) throw std::invalid_argument("Неверное имя");
+    if (name.isEmpty()) throw std::invalid_argument("Неверное имя");
     this->name = name;
-    this->students = std::vector<Student>();
+    this->students = std::vector<Student*>();
     this->dates = std::vector<QDate>();
 }
 
 void Seminar::changeSeminarName(QString name)
 {
-    if (name.length() == 0) throw std::invalid_argument("Неверное имя");
+    if (name.isEmpty()) throw std::invalid_argument("Неверное имя");
     this->name = name;
 }
 
+void Seminar::addDate(QDate date)
+{
+    for (int i = 0; i < this->dates.size(); i++)
+    {
+        if (date == dates[i]) throw std::invalid_argument("Дата уже присутствует");
+    }
+    this->dates.push_back(date);
+    sort(this->dates.begin(), this->dates.end());
+    for (int i = 0; i < this->students.size(); i++)
+    {
+        students[i]->marks[date] = 0;
+    }
+}
+
+void Seminar::deleteDate(QDate date)
+{
+    this->dates.erase(
+        std::remove(this->dates.begin(), this->dates.end(), date),
+        this->dates.end());
+
+    for (int i = 0; i < this->students.size(); i++)
+    {
+        this->students[i]->marks.erase(date);
+    }
+}
+
+Seminar::~Seminar()
+{
+    for (auto student : this->students)
+    {
+        delete student;
+    }
+}
