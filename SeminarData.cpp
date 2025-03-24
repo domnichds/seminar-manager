bd8889786e3f30cc6bb0c1d9c953f2169263f184
gremlin079 <sashdmitriev2006@gmail.com>
2025-03-24 15:36:52 +0300

    Добавить конструктор SeminarData.cpp

--- SeminarData.cpp
+++ SeminarData.cpp
@@ -7,3 +7,10 @@ SeminarData::SeminarData(QString name)
     this->dates = {};  // Инициализация пустого списка дат
     this->students = {};  // Инициализация пустого списка студентов
 }
+
+SeminarData::SeminarData(QString name, std::vector<QDate> dates, std::vector<StudentData> students)
+{
+    this->name = name;
+    this->dates = dates;
+    this->students = students;
+}
